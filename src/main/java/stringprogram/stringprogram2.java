158855db9bdfb7de29d6016da1bf6d10defa7ef7
SAROJ <shreyapurohita123@gmail.com>
2022-11-15 14:05:17 +0530

    first commit

--- src/main/java/stringprogram/stringprogram2.java
+++ src/main/java/stringprogram/stringprogram2.java
@@ -12,7 +12,7 @@ public class stringprogram2 {
         System.out.println(s1.charAt(4));
         System.out.println(s1.toUpperCase(Locale.forLanguageTag(" ")));
         System.out.println(s1.concat(s2));
-        System.out.println(s1.length());;
+        System.out.println(s1.length());
 
 
 
